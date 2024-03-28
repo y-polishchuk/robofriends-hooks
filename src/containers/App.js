ad989953454def23c03e671d516e6d337840a707
MandalorianFighter <yanush.polishchuk@gmail.com>
2024-03-28 15:01:42 +0200

    add project with hooks

---
+++ src/containers/App.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import ErrorBoundary from "../components/ErrorBoundary";
+import './App.css';
+
+function App() {
+  const [ robots, setRobots ] = useState([]);
+  const [ searchfield, setSearchfield ] = useState('');
+  const [ count, setCount ] = useState(0);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => {setRobots(users)});
+    console.log(count);
+  }, [count])
+
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value);
+  }
+
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
+    
+    return !robots.length ?
+      <h1>Loading...</h1> :
+      (
+        <div className="tc">
+          <h1 className="f2">RoboFriends</h1>
+          <button onClick={() => setCount(count + 1)}>Click Me!</button>
+          <SearchBox searchChange={onSearchChange}/>
+          <Scroll>
+            <ErrorBoundary>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundary>
+          </Scroll>
+        </div>
+      );
+}
+
+export default App;
